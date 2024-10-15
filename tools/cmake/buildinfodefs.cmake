2cc32f9b6c131bc6e376c34b5bbbc06ac3d0e401
KingSolomon <KingSolomon1954@gmail.com>
2024-10-15 16:08:18 -0700

    debugging

--- tools/cmake/buildinfodefs.cmake
+++ tools/cmake/buildinfodefs.cmake
@@ -71,7 +71,7 @@ endif()
 # Build creator whether in git tree or not
 if (DEFINED ENV{GITHUB_ACTOR})
     set(BLD_CREATOR ENV{GITHUB_ACTOR})
-elif (DEFINED ENV{LOGNAME})
+elseif (DEFINED ENV{LOGNAME})
     set(BLD_CREATOR ENV{LOGNAME})
 else()
     # We're on build container so this will always
